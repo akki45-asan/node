a78e7141d860b070f7ec7f91e7f538455630aecd
akki_patil <axypatil97@gmail.com>
2020-11-17 17:33:01 +0530

    latest

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
+const http = require('http');
 var express = require('express');
+
 var app = express();
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000 !');
+const port = process.env.PORT || 3000;
+const server = http.createServer(app);
+server.listen(port, (err) => {
+  console.log('Server  is listening on 3000');
 });
